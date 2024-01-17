bf321af5d7bcaa149cf46ead3872b6245d9eb63b
Jose Moreira Chacon <josemoreirachacon06@gmail.com>
2024-01-17 10:38:15 -0300

    At

--- core/views.py
+++ core/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required, permission_required #Obriga o suario fazer altenticação para acessar determinada pagina, que tenha a especificação "@login_required"
-from django.contrib.auth import authenticate, login, logout 
-from .models import Usuario #Importando a class
-from django.contrib.auth.models import Permission
-from .forms import UsuarioForm
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from .models import Usuario
+# Create your views here.
 
 def home(request):
     return render(request, 'index.html')
 
-@login_required
-def perfil(request):
-    return render(request, 'perfil.html')
+def cadastro(request):
+    return render(request, 'cadastro.html')
 
-def autenticar(request):
+def login(request):
+    return render(request, 'login.html')   
+
+@login_required #Para que somente usuários autenticados acessem o template perfil, em views adicionar a anotação @login_required
+def painel(request):
+    return render(request, 'painel.html')      
+
+def autenticacao(request):
     '''
     se o usuário digitou algo no formulário e clicou em enviar, o if 
     será verdadeiro, caso contrário, será uma requisição GET e entrara no else.
@@ -24,45 +30,12 @@ def autenticar(request):
        user = authenticate(request, username=usuario, password=senha)
        if user is not None:
         login(request, user)
-        return redirect('perfil')
+        return redirect('painel')
        else:
         return redirect('login') 
     else:
-        return render(request, 'registration\login.html')    
+        return render(request, 'registration\login.html') 
+
 def desconectar(request):
     logout(request)
-    return render(request, 'index.html')     
-
-@login_required
-@permission_required('core.permissao_01')
-def cursos(request):
-    return render(request, 'cursos.html')
-
-def cadastro_manual(request):
-    usuario = Usuario.objects.create_user(
-        username= 'admin5',
-        email= 'admin5@gmail.com',
-        cpf= '555',
-        nome= 'Administrador',
-        password= '123',
-        idade= 30,
-        is_superuser=False,
-    )
-
-    permissao_1= Permission.objects.get(codename='permissao_01')
-    permissao_2= Permission.objects.get(codename='permissao_02')
-
-    usuario.user_permissions.add(permissao_1, permissao_2)
-
-    usuario.save()
-    return redirect('home')
-
-def registro(request):
-    form = UsuarioForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('login')
-    context = {
-        'form': form
-    }
-    return render(request, 'registro.html', context)    
+    return render(request, 'index.html')  
